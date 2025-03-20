664a7181a17d2772ec0a5d9599281c3bfad053f3
Diego <diegofelipe.14@gmail.com>
2025-03-20 16:21:00 -0400

    maps and range

---
+++ maps.go
@@ -0,0 +1,31 @@
+// package main
+
+// import "fmt"
+
+// type Vertex struct {
+// 	Lat, Long float64
+// }
+
+// var m map[string]Vertex
+
+//	func main() {
+//		m = make(map[string]Vertex)
+//		m["Bell Labs"] = Vertex{
+//			40.68433, -74.39967,
+//		}
+//		fmt.Println(m["Bell Labs"])
+//	}
+package main
+
+import "fmt"
+
+type Position struct {
+	Lat, Long float64
+}
+
+func main() {
+	m := make(map[string]Position)
+	m["Bell Labs"] = Position{40.654546, -74.312423}
+
+	fmt.Println(m["Bell Labs"])
+}
---
+++ range.go
@@ -0,0 +1,23 @@
+// package main
+
+// import "fmt"
+
+// var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+// func main() {
+// 	for i, v := range pow {
+// 		fmt.Printf("2**%d = %d\n", i, v)
+// 	}
+// }
+
+package main
+
+import "fmt"
+
+var pow = []int{1,2,4,8,16,32,64,128}
+
+func mainranges() {
+	for i,v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+}
